33533b0802b9d7a20a623cccb53b2609ea2b399e
Nestor Castillo <nes_23@yahoo.com>
2013-07-27 19:12:23 -0400

    first commit for app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplepetrecords::Application.config.secret_token = 'e264f1be3dfc7bb165bbfc038a2ef209cd6495474692f24e9d5860149a6388136f02cbaec0c385de651f3e305070ab4eec56a042c647b1162544ea147e0a4fa2'
