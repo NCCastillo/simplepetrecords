426783595946931b71e6afc7b586248126107794
Nestor Castillo <nes_23@yahoo.com>
2013-07-27 22:54:09 -0400

    added tests and validations to user model

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
+	has_secure_password
+
+	attr_accessible :email, :password, :password_confirmation
+
+	validates_presence_of :first_name, :last_name, :email
+	validates_uniqueness_of :email
+	validates :email, :format => {:with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+	validates_length_of :password, :minimum => 6, :message => "Password must be at least 6 characers"
+
 	
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,21 +9,32 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  config.include Capybara::DSL
+  Capybara.ignore_hidden_elements = false
+
+  #config.include LoginHelper, :type => :request
+  # config.include SeederHelper, :type => :request
+  # config.include GeneralHelper, :type => :request
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each, :js => true) do
+      DatabaseCleaner.strategy = :truncation
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
