763d7b6d54352900e3fc4f55394ef734645823d8
Nestor Castillo <nes_23@yahoo.com>
2013-07-29 12:44:52 -0400

    adding integraton tests for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,18 @@ describe "StaticPages" do
   	it "should have the content 'Simple Pet Records'" do 
   		visit '/static_pages/home'
   		expect(page).to have_content('Simple Pet Records')
-  	end  
+  	end
+
+  	it "should have the content 'About us'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About us')
+  	end
+
+  	it "should have the content 'Contact us'" do 
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact us')
+  	end
   end
+
+
 end
