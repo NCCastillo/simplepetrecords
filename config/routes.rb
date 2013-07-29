53b6278690c953d4bb392922d3dd3bdf64b7b92c
Nestor Castillo <nes_23@yahoo.com>
2013-07-29 13:30:59 -0400

    added about and contact static pages along with integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Simplepetrecords::Application.routes.draw do
   root :to => 'static_pages#home'
+
   get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/contact"
   resources :users
 end
