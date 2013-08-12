70393de82a214e80f8e464d4ba00635d61587a12
Nestor Castillo <nes_23@yahoo.com>
2013-08-12 12:31:30 -0400

    got nested forms working, added edit and updated features for vets

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -2,6 +2,35 @@ class VetsController < ApplicationController
 	def new
 		@vet = Vet.new
 		@vet.build_address
+	end
 
+	def create
+		@vet = Vet.new(params[:vet])
+		pet = params[:pet_id]
+		user = params[:user_id]
+
+		if @vet.save
+			flash[:notice] = "Vet info saved"
+			redirect_to user_pet_path(user,pet)
+		else
+			flash[:notice] = "Something went wrong please try again."
+			render "new"
+		end
+	end
+
+	def edit
+		pet = Pet.find_by_id(params[:pet_id])
+		@vet = Vet.find_by_id(pet.vet.id)
+	end
+
+	def update
+		@vet = Vet.find_by_id(params[:id])
+		if @vet.update_attributes(params[:vet])
+			flash[:notice] = "Update was successful"
+			redirect_to user_pet_path(@vet.pet.user_id, @vet.pet)
+		else
+			flash[:notice] = "Something went wrong. Please try again."
+			render "edit"
+		end
 	end
 end
